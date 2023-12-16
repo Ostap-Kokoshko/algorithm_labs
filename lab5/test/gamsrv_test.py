468670c84e71b57847ee1beb096c7acf5293602c
Ostap-Kokoshko <ostap.kokoshko.ir.2022@lpnu.ua>
2023-12-16 16:28:49 +0200

    rebuild lab 5 files

--- lab5/test/gamsrv_test.py
+++ lab5/test/gamsrv_test.py
@@ -1,96 +1,54 @@
 import unittest
-import os
-
-from lab5.src.gamsrv import read_input_file, gamsrv, write_output_file
+from lab5.src.gamsrv import find_minimum_latency
 
 
-class TestGAMSRV(unittest.TestCase):
+class TestGamsrv(unittest.TestCase):
+    def test_case1(self):
+        lines = [
+            "6 6",
+            "1 2 6",
+            "1 3 10",
+            "3 4 80",
+            "4 5 50",
+            "5 6 20",
+            "2 3 40",
+            "2 4 100"
+        ]
+        expected = 100
+        actual = find_minimum_latency(lines)
+        self.assertEqual(expected, actual)
 
-    def test_case_1(self):
-        input_file_path = 'gamsrv_1.in'
-        output_file_path = 'gamsrv_1.out'
+    def test_case2(self):
+        lines = [
+            "9 12",
+            "2 4 6",
+            "1 2 20",
+            "2 3 20",
+            "3 6 20",
+            "6 9 20",
+            "9 8 20",
+            "8 7 20",
+            "7 4 20",
+            "4 1 20",
+            "5 2 10",
+            "5 4 10",
+            "5 6 10",
+            "5 8 10"
+        ]
+        expected = 10
+        actual = find_minimum_latency(lines)
+        self.assertEqual(expected, actual)
 
-        with open(input_file_path, 'w') as file:
-            file.write("6 6 \n "
-                       "1 2 6 \n"
-                       "1 3 10 \n"
-                       "3 4 80 \n"
-                       "4 5 50 \n"
-                       "5 6 20 \n"
-                       "2 3 40 \n"
-                       "2 4 100 \n")
-
-        expected_output = "100"
-
-        graph, client_list = read_input_file(input_file_path)
-        result = gamsrv(graph, client_list)
-        write_output_file(output_file_path, result)
-
-        with open(output_file_path, 'r') as file:
-            actual_output = file.read().strip()
-
-        self.assertEqual(actual_output, expected_output)
-
-        os.remove(input_file_path)
-        os.remove(output_file_path)
-
-    def test_case_2(self):
-        input_file_path = 'gamsrv_2.in'
-        output_file_path = 'gamsrv_2.out'
-
-        with open(input_file_path, 'w') as file:
-            file.write(
-                "9 12 \n"
-                "2 4 6 \n"
-                "1 2 20 \n"
-                "2 3 20 \n"
-                "3 6 20 \n"
-                "6 9 20 \n"
-                "9 8 20 \n"
-                "8 7 20 \n"
-                "7 4 20 \n"
-                "4 1 20 \n"
-                "5 2 10 \n"
-                "5 4 10 \n"
-                "5 6 10 \n"
-                "5 8 10 \n")
-
-        expected_output = "10"
-
-        graph, client_list = read_input_file(input_file_path)
-        result = gamsrv(graph, client_list)
-        write_output_file(output_file_path, result)
-
-        with open(output_file_path, 'r') as file:
-            actual_output = file.read().strip()
-
-        self.assertEqual(actual_output, expected_output)
-
-        os.remove(input_file_path)
-        os.remove(output_file_path)
-
-    def test_case_3(self):
-        input_file_path = 'gamsrv_3.in'
-        output_file_path = 'gamsrv_3.out'
-
-        with open(input_file_path, 'w') as file:
-            file.write("3 2 \n"
-                       "1 3 \n"
-                       "1 2 50 \n"
-                       "2 3 1000000000 \n")
-
-        expected_output = "1000000000"
-
-        graph, client_list = read_input_file(input_file_path)
-        result = gamsrv(graph, client_list)
-        write_output_file(output_file_path, result)
-
-        with open(output_file_path, 'r') as file:
-            actual_output = file.read().strip()
-
-        self.assertEqual(actual_output, expected_output)
-        os.remove(input_file_path)
-        os.remove(output_file_path)
+    def test_case3(self):
+        lines = [
+            "3 2",
+            "1 3",
+            "1 2 100",
+            "3 2 200"
+        ]
+        expected = 200
+        actual = find_minimum_latency(lines)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
