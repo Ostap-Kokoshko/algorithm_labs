b1803f21ba3fd013c70c67c2d66421b9498c14df
Ostap-Kokoshko <ostap.kokoshko.ir.2022@lpnu.ua>
2023-12-16 15:54:19 +0200

    adding lab 1 with zigzag_traversal and rebuild lab5 files

--- lab5/src/gamsrv.py
+++ lab5/src/gamsrv.py
@@ -1,63 +1,98 @@
-def get_neighbors(graph, vertex):
-    return graph[vertex]
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+        self.edges = {}
+
+    def add_edge(self, neighbor, weight):
+        self.edges[neighbor] = weight
 
 
-def dijkstra(start_node, graph):
-    distances = {vertex: float('infinity') for vertex in graph.keys()}
-    distances[start_node] = 0
+class Graph:
+    def __init__(self):
+        self.vertices = {}
 
-    visited = set()
-    queue = [(0, start_node)]
+    def add_vertex(self, label):
+        if label not in self.vertices:
+            vertex = Vertex(label)
+            self.vertices[label] = vertex
 
-    while queue:
-        queue.sort()
-        current_distance, node = queue.pop(0)
+    def add_edge(self, start, end, weight):
+        self.add_vertex(start)
+        self.add_vertex(end)
+        self.vertices[start].add_edge(end, weight)
+        self.vertices[end].add_edge(start, weight)
 
-        if node in visited:
+    def parse(self, lines):
+        for line in lines:
+            parts = list(map(int, line.split()))
+            start_label = parts[0]
+            for i in range(1, len(parts), 2):
+                end_label = parts[i]
+                weight = parts[i + 1]
+                self.add_edge(start_label, end_label, weight)
+
+    def dijkstra(self, start):
+        distances = {vertex: float('infinity') for vertex in self.vertices}
+        distances[start] = 0
+        priority_queue = [(0, start)]
+        visited = set()
+
+        while priority_queue:
+            priority_queue.sort()
+            current_distance, current_vertex = priority_queue.pop(0)
+
+            if current_vertex in visited:
+                continue
+
+            visited.add(current_vertex)
+
+            for neighbor, weight in self.vertices[current_vertex].edges.items():
+                distance = current_distance + weight
+
+                if distance < distances.get(neighbor, float('infinity')):
+                    distances[neighbor] = distance
+                    priority_queue.append((distance, neighbor))
+
+        return distances
+
+
+def find_minimum_latency(lines):
+    graph = Graph()
+
+    client_vertices = list(map(int, lines[1].split()))
+
+    graph.parse(lines[2:])
+
+    minimum_latency = float('infinity')
+
+    for current_vertex_label in graph.vertices:
+        if current_vertex_label in client_vertices:
             continue
 
-        visited.add(node)
+        roadmap = graph.dijkstra(current_vertex_label)
+        current_maximum_latency = max(roadmap[client_vertex] for client_vertex in client_vertices)
 
-        for neighbor, weight in get_neighbors(graph, node):
-            total_distance = current_distance + weight
+        if current_maximum_latency < minimum_latency:
+            minimum_latency = current_maximum_latency
 
-            if total_distance < distances[neighbor]:
-                distances[neighbor] = total_distance
-                queue.append((total_distance, neighbor))
-
-    return distances
+    return minimum_latency
 
 
-def gamsrv(graph, client_list):
-    result_list = []
-    for node in graph:
-        helper = dijkstra(node, graph)
-        res = max(helper[client] for client in client_list)
-        result_list.append(res)
-    return min(result_list)
+def run(input_file_name, output_file_name):
+    try:
+        with open(input_file_name, 'r') as file:
+            lines = file.read().splitlines()
+
+        minimum_latency = find_minimum_latency(lines)
+
+        with open(output_file_name, 'w') as file:
+            file.write(str(minimum_latency))
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-def read_input_file(file_path):
-    with open(file_path, 'r') as file:
-        verteces, edges = map(int, file.readline().split())
-        clients = file.readline().split()
-        graph = {str(i): [] for i in range(1, verteces + 1)}
-        for _ in range(edges):
-            start_node, end_node, latency = map(int, file.readline().split())
-            graph[str(start_node)].append((str(end_node), latency))
-            graph[str(end_node)].append((str(start_node), latency))
-
-    return graph, clients
-
-
-def write_output_file(file_path, result):
-    with open(file_path, 'w') as file:
-        file.write(str(result))
-
-
-input_file_path = 'gamsrv.in'
-output_file_path = 'gamsrv.out'
-
-graph, client_list = read_input_file(input_file_path)
-result = gamsrv(graph, client_list)
-write_output_file(output_file_path, result)
+if __name__ == "__main__":
+    input_file_name = "gamsrv.in"
+    output_file_name = "gamsrv.out"
+    run(input_file_name, output_file_name)
